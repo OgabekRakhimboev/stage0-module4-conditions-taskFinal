9e1a06319b89057c4b90fdda5a97889df9994533
OgabekRakhimboev <ogabekrakhimboev01@gmail.com>
2022-08-25 18:42:15 +0500

    Done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,50 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
-    public void printDays(int year, int month) {
+    public static void main(String[] args) {
+        printDays(2024, 2);
+    }
+    public static void printDays(int year, int month) {
+        if (year > 0 && month > 0 && month < 13){
+            switch (month){
+                default:
+                    System.out.println("wrong number!");
+                    break;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    System.out.println(31);
+                    break;
+                case 2:
+                    if (year % 4 == 0) {
+                        if (year % 100 == 0) {
+                            if (year % 400 == 0) {
+                                System.out.println(29);
+                            } else {
+                                System.out.println(28);
+                            }
+                        } else {
+                            System.out.println(29);
+                        }
+                    } else {
+                        System.out.println(28);
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println(30);
+                    break;
 
+            }
+        }
+        else {
+            System.out.println("invalid date");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
-    public void tellTheSeason(int monthNumber) {
+    public static void main(String[] args) {
+        tellTheSeason(0);
+    }
+    public static void tellTheSeason(int monthNumber) {
+        switch (monthNumber) {
+            default -> System.out.println("Wrong month number");
+            case 12, 1, 2 -> System.out.println("Winter");
+            case 3, 4, 5 -> System.out.println("Spring");
+            case 6, 7, 8 -> System.out.println("Summer");
+            case 9, 10, 11 -> System.out.println("Autumn");
+        }
     }
 }
